a1640ed4648b6e846b31090e3bd7a5b6a7eaeade
Pramila <lakadepramila@gmail.com>
2019-09-29 11:22:30 -0700

    this is my first program

---
+++ basicCoreJava/src/dataTypesandVariables/FirstJavaClass.java
@@ -0,0 +1,10 @@
+package dataTypesandVariables;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("well come to selenium");
+	}
+
+}
